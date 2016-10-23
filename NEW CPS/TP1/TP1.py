9c0926fd2387f8b675005d8424f691caa5c55e47
rfgsantos <rui.filipe.santos07@gmail.com>
2016-10-23 05:21:47 +0100

    TP1 final

--- NEW CPS/TP1/TP1.py
+++ NEW CPS/TP1/TP1.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 from matplotlib import pyplot as plt
 import scipy.io.wavfile as wav
 import sys
-import pylab as pyl
+import pylab as pylab
 # from soundPlay import soundPlay
 # print ("caminho absoluto: " + str(sys.path[0]))
 #caminho estático de gravações
@@ -16,7 +15,7 @@ fs = 8000
 ts = 1./fs
 
 #leitura do sinal gravado
-fsRecord, data = wav.read(caminho + 'novosom.wav')
+fsRecord, data = wav.read(caminho + 'sinaldevoz8khz.wav')
 
 def samplingSignal(amp, start, stop, step):
     #função retorna o array correspondente ao periodo e
@@ -81,25 +80,43 @@ def EX1B():
     plt.title('Sinal gravado')
     figure.add_subplot(212)
     #espectro do sinal gravado
-    spectrumSignal(fsRecord,newData)
+    dataABS = np.abs(newData)
+    spectrumSignal(fsRecord,dataABS)
+    amax = np.max(dataABS)
+    pylab.ylim([0,500000])
     plt.show()
     figure.savefig(caminho+'EX1B.png')
 
 def EX2A():
     newFs = 4000
     newTs = 1./newFs
-
+    #sinal normal
+    normalT, normalSignal = samplingSignal(A,0.,1.,ts)
     figure = plt.figure()
     #sinusoide amostrada a uma frequencia de 4khz
     newT, newSignal = samplingSignal(A,0.,1.,newTs)
     #grafico da sinusoide a 4khz
+    figure.add_subplot(211)
+
     plt.plot(newT, newSignal)
     plt.xlabel('Tempo')
     plt.ylabel('Amplitude')
     plt.title('Sinusoide a 4khz')
+    figure.add_subplot(212)
+    plt.plot(normalT, normalSignal)
+    plt.xlabel('Tempo')
+    plt.ylabel('Amplitude')
+    plt.title('Sinusoide a 8khz')
+    figure2 = plt.figure()
+    figure2.add_subplot(211)
+    #espectro 9khz
+    spectrumSignal(fs,normalSignal)
+    figure2.add_subplot(212)
+    #espectro 4khz
+    spectrumSignal(newFs, newSignal)
     plt.show()
-    figure.savefig(caminho+'EX2A.png')
-
+    figure.savefig(caminho+'EX2A-Tempo.png')
+    figure2.savefig(caminho+'EX2A-Espectro.png')
     recordSignal('sinal4khz.wav',newFs, newSignal)
 
 #em comparacao com o primeiro sinal podemos reparar no grafico
@@ -118,20 +135,33 @@ def EX2B():
     #array de tempo igual a amostragem
     newT = np.arange(0.,1.,1./newFs)
     #grafico do sinal de voz amostrado a uma frequencia de 1khz
+    figure2 = plt.figure()
+    #sinal de voz no tempo a 1khz
+    figure2.add_subplot(211)
     plt.plot(newT,newData_1k)
     plt.xlabel('Tempo')
     plt.ylabel('Amplitude')
     plt.title('Amostragem o sinal de voz com FS=1KHZ')
     recordSignal('sinaldevoz1khz.wav',newFs,newData_1k)
+    figure2.add_subplot(212)
+    #sinal de voz no tempo a 8khz
+    Trecord = np.arange(0.,1.,1./fsRecord)
+    plt.plot(Trecord, data[:fsRecord])
+    plt.xlabel('Tempo')
+    plt.ylabel('Amplitude')
+    plt.title('Amostragem o sinal de voz com FS=8KHZ')
+    figure2.savefig(caminho + 'EX2B-Tempo.png')
     figure = plt.figure()
     figure.add_subplot(211)
     #espectro do sinal de voz amostrado a 1khz
-    spectrumSignal(newFs, newData_1k)
+    spectrumSignal(newFs, np.abs(newData_1k))
+    pylab.ylim([0,100000])
     figure.add_subplot(212)
     #espectro do sinal de voz amostrado a 8khz
-    spectrumSignal(fsRecord, data[:fsRecord])
+    spectrumSignal(fsRecord, np.abs(data[:fsRecord]))
+    pylab.ylim([0,500000])
     plt.show()
-    figure.savefig(caminho + 'EX2B.png')
+    figure.savefig(caminho + 'EX2B-Espectro.png')
 
 
 if __name__ == "__main__":
