bde07e66e2eb5da67d3c1a607ad8bb2db4c49df1
dmpfernandes <dmpf130@gmail.com>
2016-10-24 17:50:49 +0100

    funçoes terminadas

--- NEW CPS/TP2/TP2-diogo.py
+++ NEW CPS/TP2/TP2-diogo.py
@@ -10,14 +10,26 @@ def createTable(R, Vmax):
     return valoresDecisao, niveisQuantificacao
 
 
-x,y=createTable(R,Vmax)
+VD,NQ=createTable(R,Vmax)
 
-print "Niveis de Quantificação : " + str(y)
-print "Valores de Decisão : " + str(x)
+print "Niveis de Quantificação : " + str(NQ)
+print "Valores de Decisão : " + str(VD)
 
 
 def quantificacao(sinalAmostrado, NQ, VD):
-    sinalresultante = np.zeros(len(sinalAmostrado))
-    for a in sinalAmostrado:
-        indice = a < VD
-        indice = indice*1.
+    sinalQuantificado = np.zeros(len(sinalAmostrado))
+    aux = np.zeros(len(sinalAmostrado))
+    for a in range(len(sinalAmostrado)):
+        indice = sinalAmostrado[a] < VD
+        print "indice: " + str(indice)
+        indiceTrue = np.nonzero(indice)
+        print "indiceTrue: " + str(indiceTrue[0])
+        aux[a] = indiceTrue[0][0]
+        sinalQuantificado[a] = NQ[aux[a]-1]
+
+    return sinalQuantificado , aux
+
+SQ,VQ=quantificacao([0.3,0.6,-0.3,-.6,.8] , NQ, VD)
+
+print "sinalQuantificado: " + str(SQ)
+print "Valores de Quantificação utilizados: " + str(VQ)
