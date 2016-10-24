032d3a224cfc81bf87b7b75d70bb1219c5328d0f
RuiSantos <rui.filipe.santos07@gmail.com>
2016-10-24 15:39:02 +0100

    TP2 cps

---
+++ NEW CPS/TP2/TP2.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.io.wavfile as wav
+import sys
+import pylab as pylab
+
+
+def tabelas(R, Vmax):
+    #sendo R o numero de bits
+    #Vmax a voltagem maxima do sinal
+    L = 2**R
+    deltaQ = (2*Vmax) / L
