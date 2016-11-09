173b8c3856f67021d0fc3e332a02e1c725e61e86
rfgsantos <rui.filipe.santos07@gmail.com>
2016-11-09 02:57:20 +0000

    erros ainda por resolver

--- NEW CPS/TP2/TP2-diogo.py
+++ NEW CPS/TP2/TP2-diogo.py
@@ -13,17 +13,17 @@ caminho = str(sys.path[0])+"\\"
 #-------------------Funções-------------------
 #calcula a potencia do sinal amostrado
 def potenciaSinal(sinalAmostrado):
-    potencia = (1./len(sinalAmostrado))*np.sum(sinalAmostrado**2.)
+    potencia = (1./len(sinalAmostrado))*np.sum(sinalAmostrado**2)
     return potencia
 
 #calcula a potencia do sinal de erro de quantificação
 def potenciaErroQuant(erroQuantificado):
-    potenciaErro = (1./len(erroQuantificado))*np.sum(erroQuantificado**2.)
+    potenciaErro = (1./len(erroQuantificado))*np.sum(erroQuantificado**2)
     return potenciaErro
 
 #calcula o SNR teorico
 def SNRTeorico(potencia,R,Vmax):
-    valor = 6.s*R + 10.*np.log((3.*potencia)/Vmax**2.)
+    valor = 6*R + 10.*np.log((3.*potencia)/Vmax**2)
     return valor
 
 #calcula o SNR pratico
@@ -35,6 +35,10 @@ def SNRPratico(potenciaSinal, potenciaErro):
 def erroQuantificacao(sinalOriginal, sinalQuantificado):
     erroSinal = sinalOriginal - sinalQuantificado
     return erroSinal
+
+#grava um sinal
+def recordSignal(name, freq, signal):
+    wav.write(caminho+name,freq,signal.astype('int16'))
 
 #retorna dois numpy arrays com os valores de
 #decisão e com os niveis de quantificação
@@ -59,10 +63,10 @@ def quantificacao(sinalAmostrado, NQ, VD):
             aux = int(indiceTrue[0][0]-1)
             indiceQuant[a] = aux
             sinalQuantificado[a] = NQ[aux]
-    return sinalQuantificado , indiceQuant.astype('int16')
+    return sinalQuantificado.astype('int16') , indiceQuant.astype('int16')
 
 #Codifica em binario o array de Indices
-#retorna um numpy array com os valores em binario a 3 bit
+#retorna um numpy array com os valores em binario a R bits
 #os valores do array de indices
 def codificaSinal(IQ, R):
     sinalCodificado = np.zeros(len(IQ)*R)
@@ -70,95 +74,34 @@ def codificaSinal(IQ, R):
     binario = '{0:0'+str(R)+'b}'
     for i in range(len(IQ)):
         aux = binario.format(IQ[i])
-        sinalCodificado[count]=int(aux[0])
-        sinalCodificado[count+1]=int(aux[1])
-        sinalCodificado[count+2]=int(aux[2])
-        count+=3
+        for x in range(0,R):
+            sinalCodificado[count+x]=int(aux[x])
+        count+=R
     return sinalCodificado.astype('int16')
 
 
-
-def recordSignal(name, freq, signal):
-    wav.write(caminho+name,freq,signal.astype('int16'))
-
-#-------------Reverse MotherFucker-------------------
-
 def descodificaSinal(sinalCodificado, R):
-    sinalDescodificado = np.zeros(len(sinalCodificado)/R)
+    size = int(len(sinalCodificado)/R)
+    print("tamanho do sinal codificado: " + str(len(sinalCodificado)))
+    print("inteiro do tamanho do array: " + str(size))
+    sinalDescodificado = np.zeros(size)
     count = 0
     binario = ''
     for i in range(0,len(sinalCodificado),R):
         for x in range(0,R):
-            binario += str(sinalCodificado[x])
+            binario += str(sinalCodificado[i+x])
         sinalDescodificado[count]=int(binario,2)
+        binario=''
         count+=1
 
     return sinalDescodificado.astype('int16')
+
 
 def quantificacaoInversa(sinalDescodificado,niveisQuantificacao):
     sinalQuantInv = np.zeros(len(sinalDescodificado))
     for i in range(len(sinalDescodificado)):
         sinalQuantInv[i]=niveisQuantificacao[sinalDescodificado[i]]
     return sinalQuantInv
-
-def CORRETUDO():
-    #--------------------Variaveis-----------------------
-    R = 8
-    Vmax = 1
-    SR=np.arange(-1,1,0.0001)
-    fs = 1000
-    f = 3014.
-    T = np.arange(0,1,1/fs)
-    A = 1000
-
-    y = A*np.cos(2*np.pi*f*T)
-
-    #--------------Execução de funções-------------------
-    VD,NQ = createTable(R,np.amax(abs(SR)))
-    SQ,IQU = quantificacao(SR , NQ, VD)
-    potencia = potenciaSinal(SR)
-    erro = erroQuantificacao(SR,SQ)
-    potenciaErro = potenciaErroQuant(erro)
-    sinalCodificado = codificaSinal(IQU, R)
-    sinalDescodificado = descodificaSinal(sinalCodificado,R)
-    sinalQuantInv = quantificacaoInversa(sinalDescodificado,NQ)
-    recordSignal('sinosoide.wav',fs,SR)
-    recordSignal('sinalQuantificado.wav',fs,SQ)
-    recordSignal('sinalQuantInv.wav',fs,sinalQuantInv)
-
-
-    #----------------------Prints------------------------
-    print ("------------------------------------------------------------------------")
-    print ("Niveis de Quantificacao: \n" + str(NQ))
-    print ("------------------------------------------------------------------------")
-    print ("Valores de Decisao: \n" + str(VD))
-    print ("------------------------------------------------------------------------")
-    print ("Potencia do sinal: " + str(potencia))
-    print ("------------------------------------------------------------------------")
-    print ("sinalQuantificado: \n" + str(SQ))
-    print ("------------------------------------------------------------------------")
-    print ("Indices de Quantificacao utilizados: \n" + str(IQU))
-    print ("------------------------------------------------------------------------")
-    print ("Erro de Quantificacao: \n" + str(erro))
-    print ("------------------------------------------------------------------------")
-    print ("Potencia do Erro de Quantificacao: \n" + str(potenciaErro))
-    print ("------------------------------------------------------------------------")
-    print ("Codificacao: \n" + str(sinalCodificado))
-    print ("------------------------------------------------------------------------")
-    print ("Descodificacao: \n" + str(sinalDescodificado))
-    print ("------------------------------------------------------------------------")
-    print ("Quantificacao Inversa: \n" + str(sinalQuantInv))
-    print ("------------------------------------------------------------------------")
-
-
-
-    #-----------------------Plots-----------------------
-    # Tsq=np.arange(0,len(SQ))
-    # plt.grid(True)
-    # plt.plot(Tsq,SQ, label='SinalQuantificado')
-    # plt.plot(Tsq,SR, label='SinalRampa')
-    # plt.legend(loc='lower right')
-    # plt.show()
 
 
 def EX3AQ():
@@ -215,7 +158,6 @@ def EX4AQ():
 
 def EX4BQ():
     fsRecord, data = wav.read(caminho + 'sinaldevoz8khz.wav')
-
     VD,NQ = createTable(8,np.amax(abs(data)))
     SQ,IQU = quantificacao(data , NQ, VD)
     SQanterior = np.zeros(len(SQ))
@@ -258,8 +200,43 @@ def EX4CQ():
     plt.show()
 
 def EX2C():
-    R=[3,4,5,6,7,8]
+    R=[3,5,8]
+    fsRecord, data = wav.read(caminho + 'sinaldevoz8khz.wav')
+    arraySNRpratico = np.zeros(len(R))
+    arraySNRteorico = np.zeros(len(R))
 
+    for i in range(len(R)):
+        VD,NQ = createTable(R[i],np.amax(abs(data)))
+        #quantifica o sinal
+        SQ,IQU = quantificacao(data , NQ, VD)
+        erro = erroQuantificacao(data,SQ)
+        potenciaErro = potenciaErroQuant(erro)
+        potencia = potenciaSinal(data)
+        auxSNRP = SNRPratico(potencia,potenciaErro)
+        arraySNRpratico[i] = auxSNRP
+        auxSNRT = SNRTeorico(potencia,R[i],np.amax(abs(data)))
+        arraySNRteorico[i] = auxSNRT
+        #codificar sinal
+        sinalCodificado = codificaSinal(IQU,R[i])
+        #descodifica sinal
+        sinalDescodificado = descodificaSinal(sinalCodificado,R[i])
+        #quantificaçao inversa do sinal
+        sinalDesquantificado = quantificacaoInversa(sinalDescodificado,IQU)
+        #gravacao do sinal
+        recordSignal("Sinal R="+str(R[i])+".wav",fsRecord,sinalDesquantificado)
+
+    B = np.vstack((arraySNRteorico,arraySNRpratico))
+    fig,axs =plt.subplots(2,1)
+    clust_data = B
+    collabel=("R=3", "R=5", "R=8")
+    rowlabel=("SNRTeorico","SNRPratico")
+    axs[0].axis('off')
+    the_table = axs[0].table(cellText=clust_data,colLabels=collabel,rowLabels=rowlabel,loc='center')
+
+    axs[1].plot(R,arraySNRteorico,'o',label='SNRteorico')
+    axs[1].plot(R,arraySNRpratico,'o',label='SNRpratico')
+    axs[1].legend(loc='upper left')
+    plt.show()
 
 if __name__=="__main__":
     # EX3AQ()
@@ -267,7 +244,8 @@ if __name__=="__main__":
     # EX3CQ()
     # EX4AQ()
     # EX4BQ()
-    EX4CQ()
+    # EX4CQ()
+    EX2C()
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
