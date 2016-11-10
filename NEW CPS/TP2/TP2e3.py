129aeef5c17c06fad99bb095b3dbe84ba583ea26
rfgsantos <rui.filipe.santos07@gmail.com>
2016-11-10 03:30:41 +0000

    tp com erros na codif e descodif

--- NEW CPS/TP2/TP2e3.py
+++ NEW CPS/TP2/TP2e3.py
@@ -63,7 +63,7 @@ def quantificacao(sinalAmostrado, NQ, VD):
             aux = int(indiceTrue[0][0]-1)
             indiceQuant[a] = aux
             sinalQuantificado[a] = NQ[aux]
-    return sinalQuantificado.astype('int16') , indiceQuant.astype('int16')
+    return sinalQuantificado , indiceQuant.astype('int16')
 
 #Codifica em binario o array de Indices
 #retorna um numpy array com os valores em binario a R bits
@@ -110,11 +110,15 @@ def EX3AQ():
     VD,NQ = createTable(R,np.amax(abs(SR)))
     SQ,IQU = quantificacao(SR , NQ, VD)
     Tsq=np.arange(0,len(SQ))
+    figure = plt.figure()
     plt.grid(True)
     plt.plot(Tsq,SQ, label='SinalQuantificado')
     plt.plot(Tsq,SR, label='SinalRampa')
+    plt.xlabel("Tempo")
+    plt.ylabel("Valores de quantificacao")
     plt.legend(loc='lower right')
     plt.show()
+    figure.savefig(caminho + 'EX3AQ.png')
 
 def EX3BQ():
     R = 3
@@ -122,9 +126,19 @@ def EX3BQ():
     VD,NQ = createTable(R,np.amax(abs(SR)))
     SQ,IQU = quantificacao(SR , NQ, VD)
     erro = erroQuantificacao(SR,SQ)
+    figure = plt.figure()
+    erroTempo = np.arange(0,len(erro))
+    plt.plot(erroTempo,erro)
+    plt.ylabel("Valores de Erro")
+    plt.xlabel("Tempo")
+    figure.savefig(caminho + 'EX3BQ.png')
+    figure2 = plt.figure()
     plt.hist(erro)
+    plt.ylabel("Numero de vezes erro")
+    plt.xlabel("Valores erro")
     pylab.ylim([1950,2050])
     plt.show()
+    figure2.savefig(caminho + 'EX3BQ-2.png')
 
 def EX3CQ():
     R = [3,4,5,6,7,8]
@@ -143,32 +157,39 @@ def EX3CQ():
         arraySNRteorico[i] = auxSNRT
 
     B = np.vstack((arraySNRteorico,arraySNRpratico))
-    fig,axs =plt.subplots(1,1)
+    fig,axs =plt.subplots(2,1)
     clust_data = B
     collabel=("R=3", "R=4", "R=5", "R=6", "R=7", "R=8")
     rowlabel=("SNRTeorico","SNRPratico")
-    axs.axis('off')
-    the_table = axs.table(cellText=clust_data,colLabels=collabel,rowLabels=rowlabel,loc='center')
+    axs[0].axis('off')
+    the_table = axs[0].table(cellText=clust_data,colLabels=collabel,rowLabels=rowlabel,loc='center')
+    axs[1].plot(R,arraySNRteorico,'o',label='SNRteorico')
+    axs[1].plot(R,arraySNRpratico,'o',label='SNRpratico')
+    axs[1].legend(loc='upper left')
     plt.show()
+    fig.savefig(caminho + 'EX3CQ.png')
 
 def EX4AQ():
     fsRecord, data = wav.read(caminho + 'sinaldevoz8khz.wav')
+    figure = plt.figure()
     plt.hist(data)
     plt.show()
+    figure.savefig(caminho + 'EX4AQ.png')
 
 def EX4BQ():
     fsRecord, data = wav.read(caminho + 'sinaldevoz8khz.wav')
     VD,NQ = createTable(8,np.amax(abs(data)))
     SQ,IQU = quantificacao(data , NQ, VD)
     SQanterior = np.zeros(len(SQ))
-
+    figure = plt.figure()
     for i in range(len(SQ)):
         if(i==len(SQ)-1):
             break
         SQanterior[i+1] = SQ[i]
 
-    plt.plot(SQanterior,SQ,'o r')
+    plt.plot(SQanterior,SQ,'o b')
     plt.show()
+    figure.savefig(caminho + 'EX4BQ.png')
 
 def EX4CQ():
     R = [3,4,5,6,7,8]
@@ -193,11 +214,12 @@ def EX4CQ():
     rowlabel=("SNRTeorico","SNRPratico")
     axs[0].axis('off')
     the_table = axs[0].table(cellText=clust_data,colLabels=collabel,rowLabels=rowlabel,loc='center')
-
     axs[1].plot(R,arraySNRteorico,'o',label='SNRteorico')
     axs[1].plot(R,arraySNRpratico,'o',label='SNRpratico')
     axs[1].legend(loc='upper left')
     plt.show()
+
+    fig.savefig(caminho + 'EX4CQ.png')
 
 def EX2C():
     R=[3,5,8]
@@ -232,11 +254,11 @@ def EX2C():
     rowlabel=("SNRTeorico","SNRPratico")
     axs[0].axis('off')
     the_table = axs[0].table(cellText=clust_data,colLabels=collabel,rowLabels=rowlabel,loc='center')
-
     axs[1].plot(R,arraySNRteorico,'o',label='SNRteorico')
     axs[1].plot(R,arraySNRpratico,'o',label='SNRpratico')
     axs[1].legend(loc='upper left')
     plt.show()
+    fig.savefig(caminho + 'EX2C.png')
 
 if __name__=="__main__":
     # EX3AQ()
@@ -245,7 +267,7 @@ if __name__=="__main__":
     # EX4AQ()
     # EX4BQ()
     # EX4CQ()
-    EX2C()
+    # EX2C()
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
