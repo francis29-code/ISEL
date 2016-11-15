68aac454535d93ade3766770886586b40be75bad
RuiSantos <rui.filipe.santos07@gmail.com>
2016-11-15 16:41:46 +0000

    error

---
+++ NEW CPS/TP3/TP3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.io.wavfile as wav
+import sys
+import pylab as pylab
+import time
+
+start_time = time.time()
+
+caminho = str(sys.path[0])+"\\"
