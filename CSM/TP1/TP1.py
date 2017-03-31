c8117b46debad92ce98c5cdfd8e717dd27b65b2f
rui santos <rui.filipe.santos07@gmail.com>
2017-04-01 00:13:38 +0100

    tp1 almost finish

--- CSM/TP1/TP1.py
+++ CSM/TP1/TP1.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 import sys
+import os
 path = str(sys.path[0])+"\\"
 
 def saveImage(nome,image,quality=100,formato=".jpg"):
@@ -14,20 +15,57 @@ def readImage(nome):
     img = cv.imread(path+nome)
     return img
 
+def showImage(titulo,img):
+    cv.imshow("Titulo: {}".format(titulo),img)
+
+def imageSNR(original,amplificada):
+    imageO = original.ravel()
+    noise = original.astype(float) - amplificada.astype(float)
+    imageA = noise.ravel()
+
+    pOriginal = np.sum(imageO**2.)
+    pAmplificada = np.sum(imageA**2.)
+    SNR = 10*np.log10(pOriginal/pAmplificada)
+
+    return SNR
+
+def MSE(original,amplificada):
+    linhas = len(original)
+    colunas = len(original[0])
+    noise = original.astype(float) - amplificada.astype(float)
+    noiseR = noise.ravel()
+    pAmplificada = np.sum(noiseR**2.)
+    MSE = (1./(linhas*colunas))*pAmplificada
+
+    return MSE
+
+def PSNR(MSE):
+    PSNR = 20*np.log10(255) - 10*np.log10(MSE)
+    return PSNR
+
+def compressionRatio(original,amplificada):
+    sizeO = os.path.getsize(path+original)
+    sizeA = os.path.getsize(path+amplificada)
+
+    ratio = sizeO/sizeA
+
+    return int(ratio)
+
 
 def ex1():
-    img = cv.imread(path+'lenac.tif')
-    cv.imshow('Original Image', img)
+    img = readImage('lenac.tif')
+    showImage('Original Image', img)
     #indica-nos o datatype
-    print (img.dtype)
-    #indica-nos
-    print (img.shape)
+    print ("Datatype: {}".format(img.dtype))
+    #indica-nos o code ex UTF8
+    print ("Shape: {}".format(img.shape))
+    print ("Shape rows: {} | columns: {}".format(img.shape[0],img.shape[1]))
     cv.waitKey(0)
     cv.destroyAllWindows()
 
 def ex2():
     #leitura da imagem incial
-    img = cv.imread(path+'lenac.tif')
+    img = readImage('lenac.tif')
     #gravação das duas imagens com diferentes percentagens de qualidade
     saveImage('imagem',img,80)
     saveImage('imagem',img,10)
@@ -35,43 +73,45 @@ def ex2():
     img80 = readImage('imagem - 80.jpg')
     img10 = readImage('imagem - 10.jpg')
     ## visualização das duas imagens gravadas
-    cv.imshow('Imagem 80', img80)
-    cv.imshow('Imagem 10', img10)
+    showImage('Imagem 80', img80)
+    showImage('Imagem 10', img10)
+
     #um pequeno "waiting" para a visualização das imagens
     cv.waitKey(0)
 
     #taxa de compressão é a divisão entre a original e a comprimida
-
+    ratio10 = compressionRatio('lenac.tif','imagem - 10.jpg')
+    print("\nRatio da imagem 10: {}%".format(ratio10))
+    ratio80 = compressionRatio('lenac.tif','imagem - 80.jpg')
+    print("Ratio da imagem 80: {}%".format(ratio80))
 
     #calculo da SNR
-    imgR = img.ravel()
-    dif = img.astype(float)-img10.astype(float)
-    difR = dif.ravel()
-    pOriginal = np.sum(imgR**2.)
-    pRuido = np.sum(difR**2.)
-    SNR  = 10*np.log10(pOriginal/pRuido)
-    print("\nSNR - dB - img10 -> {}".format(SNR))
-
-    # SNR  = 10*np.log10()
-    # print("\nSNR - dB -> {}".format(SNR))
-
+    SNR10  = imageSNR(img,img10)
+    print("\nSNR dB - img10 -> {}dB".format(SNR10))
+    SNR80 = imageSNR(img,img80)
+    print("SNR dB img80 -> {}dB".format(SNR80))
 
     #calculo MSE
-    MSE = 1/(len(img)*len(img[0])) * (np.sum(img-img10)**2)
-    print("\nMSE -> {}".format(MSE))
+    MSE10 = MSE(img,img10)
+    print("\nMSE - img10 -> {}".format(MSE10))
+    MSE80 = MSE(img,img80)
+    print("MSE - img80 -> {}".format(MSE80))
+
     #calculo PSNR
-    PSNR = 10*np.log10(255/MSE)
-    print("\nPSNR -> {}".format(PSNR))
+    PSNR10 = PSNR(MSE10)
+    print("\nPSNR - img10 -> {}dB".format(PSNR10))
+    PSNR80 = PSNR(MSE80)
+    print("PSNR - img80 -> {}dB".format(PSNR80))
 
 
 def ex3():
     # Y = R299=1000+G587=1000+B114=1000
     #leitura da imagem
-    img = cv.imread(path+'lenac.tif')
+    img = readImage('lenac.tif')
     #transformação da imagem para escala de cinzento
     imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     #visualização da imagem
-    cv.imshow('Escala Cinzentos',imgGray)
+    showImage('Escala Cinzentos', imgGray)
     #guardar imagem
     saveImage('escalaCinzentos',imgGray,100,'.bmp')
 
@@ -84,15 +124,71 @@ def ex4():
     plt.hist(img.ravel(),256,[0,256])
     plt.show()
 
+    #cria array de valores UNICOS do histograma
     arrayUnique = np.unique(img)
     print("Niveis Cinzentos: {} ".format(len(arrayUnique)))
+
+def ex5():
+    #leitura imagem
+    img = readImage('escalaCinzentos - 100.bmp')
+    count=0
+    for i in range(8):
+        count += (2.**i)
+        imageBit = img > count
+        showImage('BW - bit: {}'.format(i),imageBit*1.)
+
+    cv.waitKey(0)
+
+def ex6():
+
+    img = readImage('escalaCinzentos - 100.bmp')
+
+    imageBit = img << 4
+
+    showImage('BW - 4 bits',imageBit*1)
+
+    cv.waitKey(0)
+
+
+def ex7(angle):
+    #width & height
+    w ,h = (400,400)
+    #criação do array da imagem
+    img = np.ones((w,h,3),np.uint8)
+
+    #uso da , indica que estamos a selecionar uma lista na posicação do slice
+    print("Valores Linhas: {} | Colunas: {}".format(img.shape[0],img.shape[1]))
+
+    #fundo branco
+    img[::] = (255,255,255)
+
+    #centro da imagem
+    center = (int(w/2),int(h/2))
+
+    #limit
+    times = int(360/angle)
+
+    #angulo dado pelo utilizador
+    for i in range(times):
+        newCenterX = int(np.round(center[0] + w * np.cos((i*angle) * np.pi / 180.0)))
+        newCenterY = int(np.round(center[1] + w * np.sin((i*angle) * np.pi / 180.0)))
+        newCenter = (newCenterX,newCenterY)
+
+        cv.line(img,center,newCenter,(0,0,0),2)
+
+    showImage('Imagem Criada',img)
+
+    cv.waitKey(0)
+
+    print("ex7")
+
 
 
 if __name__ == "__main__":
     # ex1()
-    ex2()
+    # ex2()
     # ex3()
     # ex4()
-
-    # b = np.array([[2,3,4,4],[2,3,4,5]])
-    # print("length linhas: {} - length colunas: {}".format(len(b),len(b[0])))
+    # ex5()
+    # ex6()
+    ex7(1)
