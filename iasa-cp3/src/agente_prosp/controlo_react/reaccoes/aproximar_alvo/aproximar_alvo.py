36febfe1949ab31020818f9d052df8a22dd8539f
Rui <rui.filipe.santos07@gmail.com>
2017-05-07 13:48:16 +0100

    iasa cp3

--- iasa-cp3/src/agente_prosp/controlo_react/reaccoes/aproximar_alvo/aproximar_alvo.py
+++
@@ -1,9 +0,0 @@
-from Prioridade import Prioridade
-from aproximar_alvo_dir import AproximarAlvoDir
-
-class AproximarAlvo(Prioridade):
-    
-    def __init__(self):
-        super(AproximarAlvo, self).__init__([AproximarAlvoDir("ESQ"),
-                                             AproximarAlvoDir("FRT"),
-                                             AproximarAlvoDir("DIR")])
