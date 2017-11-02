95d23471b4c7e4fd10389b72fef1e8eb296cddbf
ruifgsantos <rui.filipe.santos07@mail.com>
2017-11-02 00:56:05 +0000

    piv almost ending

--- PIV/TP1/tp1.py
+++ PIV/TP1/tp1.py
@@ -3,27 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+global valor
+valor = 0
 
-def teste():
+moedas = {'0.01':0.01, '0.02':0.02, '0.05':0.05,
+        '0.10':0.10, '0.20':0.20, '0.50':0.50, '1.00':1.00}
 
-    #leitura da imagem em tons de cinzento
-    img = cv2.imread('P1.jpg',cv2.IMREAD_GRAYSCALE)
+def improveImage(image):
+    return cv2.bilateralFilter(image,22,30,30)
 
-    #smoothing a imagem - MELHORAMNETO
-    melhorada = cv2.bilateralFilter(img,22,30,30)
+def imageBinary(image):
+    return cv2.threshold(image,0,255,cv2.THRESH_TRIANGLE)
 
-    #THRESH_TRIANGLE fica mais nitida - BINARIZACAO
-    rt,thresh = cv2.threshold(melhorada,0,255,cv2.THRESH_TRIANGLE)
-
+def smoothImage(image):
     #TRATAMENTO DA IMAGEM
     #structuring element - uma imagem binaria
-    matriz = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
+    matriz = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
     #dilate melhor que o erode, imagens das moedas ficam mais redondas
-    erode = cv2.erode(thresh,matriz,iterations=1)
+    erode = cv2.erode(image,matriz,iterations=1)
+    return erode
 
+def centroides(image):
     #apanhar os contornos de uma imagem binaria
-    contornos,hierarquia,lel = cv2.findContours(erode,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(img,contornos,0,(0,255,0),cv2.FILLED,8,hierarquia[0])
+    contornos,hierarquia,lel = cv2.findContours(image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     centroide = []
     cx = []
     cy = []
@@ -35,39 +37,67 @@ def teste():
                 cx.append(int(momentos['m10']/momentos['m00']))
                 cy.append(int(momentos['m01']/momentos['m00']))
                 centroide.append(conta)
+    return centroide,cx,cy
 
-    aux = []
+
+def distToCenter(centroide,cx,cy):
+    corrected = []
     #maxima diferenca do raio calculado inicialmente e os pontos
     #provenientes dos contornos
-    threshold_maximo = 10
+    mx = 10
     counter =0
     #distance to center
-    print centroide[0] #array
-    print "1" + str(centroide[0][0])        #primeiro ponto
-    print "2" + str(centroide[0][0][0])     #tuplo
-    print "3-" + str(centroide[0][0][0][0]) #x
-    print "4-" + str(centroide[0][0][0][1]) #y
-
     for x,y in zip(cx,cy):
-        distref = np.sqrt(cv2.contourArea(hierarquia[counter])/np.pi)
-        current = hierarquia[counter]
+        classifica(cv2.contourArea(centroide[counter]))
+        distref = np.sqrt(cv2.contourArea(centroide[counter])/np.pi)
+        distref = distref+mx
+        distref2 = distref-(2*mx)
+        current = centroide[counter]
+        filtered = filter(lambda f: True if (np.sqrt((f[0][0]-x)**2 + (f[0][1]-x)**2)>distref
+        or np.sqrt((f[0][0]-x)**2 + (f[0][1]-x)**2)<distref2) else False, current)
+        corrected.append(filtered)
+        counter+=1
 
-        print cal
+    return corrected
 
+def classifica(area):
+    global valor
+    if(area>= 7000 and area<=10000):
+        valor += moedas.get('0.01')
+    if(area>= 11000 and area<12000):
+        valor += moedas.get('0.02')
+    if(area>= 14000 and area<=15000):
+        valor += moedas.get('0.05')
+    if(area>= 12000 and area<14000):
+        valor += moedas.get('0.10')
+    if(area> 15000 and area<=17000):
+        valor += moedas.get('0.20')
+    if(area> 19000 and area<=23000):
+        valor += moedas.get('0.50')
+    if(area> 17000 and area<=19000):
+        valor += moedas.get('1.00')
 
+def teste():
 
+    #leitura da imagem em tons de cinzento
+    img = cv2.imread('P1.jpg',cv2.IMREAD_GRAYSCALE)
+    # cv2.imshow('Imagem Original',img)
+    #smoothing a imagem - MELHORAMNETO
+    melhorada = improveImage(img)
+    # cv2.imshow('Imagem Melhorada',melhorada)
+    #THRESH_TRIANGLE fica mais nitida - BINARIZACAO
+    rt,thresh = imageBinary(melhorada)
+    cv2.imshow('Imagem Binarizada',thresh)
+    #erosao
+    erode = smoothImage(thresh)
+    cv2.imshow('Imagem Erodida',erode)
+    #centroides
+    centroide,cx,cy = centroides(erode)
+    #filtrar pontos que nao estao de acordo com o raio esperado
+    array_final = distToCenter(centroide,cx,cy)
+    cv2.waitKey(0)
 
-        break
-
-
-
-    #hierarquia sao o numero de figuras encontradas com o structuring element
-    #definido na dilataao da imagem
-    # new_contornos = [(lambda x: True if any(x)!=False else False)(x) for x in hierarquia[0]]
-    # new_contornos = [x for x in hierarquia: if ]
-
-    # cv2.imshow("thresholded",erode)
-    # cv2.waitKey(0)
+    print "Valor final: " + str(valor)
 
 if __name__ == "__main__":
     teste()
